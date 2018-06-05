4061ae662494b4f565bf5380655b75786953bbbc
whatislife <1053400192@qq.com>
2018-06-05 14:42:11 +0800

    测试数据

--- src/test/java/zhhtDemo/TestMLocal.java
+++ src/test/java/zhhtDemo/TestMLocal.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import com.zhht.util.HttpUtil;
 import com.zhht.util.SignatureUtil;
-public class TestM {
+public class TestMLocal {
 	@Test
 	public void queryParkMessage(){
 		String url = "http://localhost:8083/zhht_cps/cps/park/queryParkMessage";
@@ -30,7 +30,7 @@ public class TestM {
 		//测试阶段使用
 		String tokenKey1="RlE3RUdONUlOQVoyWUZHVkZQN0lHUUlSUUJQNU9GTzFUVlFSWVBWUEFJWERQTks0UUFVRVBXV0dVRkhVSUtDRjdZRENISFZW";
 		String tokenKey2="NFVDSlQwNExMUVVDMkEzRzlWSFNETFFLVDdOS0ozWjBOS1VYSEhDWVhYVVkxV0tBSVNSTlFSV0lZSktYRTJYR05PSFVZSjdT";
-		String source = "{'parkCode': 'PA2018110','beginTime ': '2017-12-07 16:06:57','endTime': '2017-12-20 16:06:57'}";
+		String source = "{'parkCode': 'PA20171201141602','beginTime': '2018-05-09 16:47:47','endTime': '2018-05-09 16:47:55'}";
 		try {
 			String token = SignatureUtil.getMD5(SignatureUtil.getMD5(source + tokenKey1) + tokenKey2);
 			System.out.println(token);
